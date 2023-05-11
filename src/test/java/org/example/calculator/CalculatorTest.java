f2cba39bc7ae26b77db2a77de0719d56dc2732db
Booki Park <bkbkgg@gmail.com>
2023-05-11 20:31:09 +0900

    인터페이스와 구현체를 통해 연산 메서드 분리

--- src/test/java/org/example/calculator/CalculatorTest.java
+++ src/test/java/org/example/calculator/CalculatorTest.java
@@ -1,7 +1,7 @@
 package org.example.calculator;
 
-import org.example.calculator.Calculator;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -9,6 +9,7 @@ import org.junit.jupiter.params.provider.MethodSource;
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 /**
@@ -34,5 +35,13 @@ public class CalculatorTest {
                 arguments(4, "*", 2, 8),
                 arguments(4, "/", 2, 2)
         );
+    }
+
+    @DisplayName("나눗셈에서 0을 나누는 경우 IllegalArgument 예외를 발생시킨다.")
+    @Test
+    void calculateExceptionTest() {
+        assertThatCode(() -> Calculator.calculate(10, "/", 0))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("0으로 나눌 수 없습니다");
     }
 }
