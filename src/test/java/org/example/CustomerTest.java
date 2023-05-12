5ac8c02ab36fbfc12131ee42e4a7147fd5747933
Booki Park <bkbkgg@gmail.com>
2023-05-12 15:36:28 +0900

    손님의 주문 테스트

--- src/test/java/org/example/CustomerTest.java
+++ src/test/java/org/example/CustomerTest.java
@@ -1,11 +1,21 @@
 package org.example;
 
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 public class CustomerTest {
 
+    @DisplayName("메뉴 이름에 해당하는 요리를 주문을 한다.")
     @Test
-    void name() {
+    void orderTest() {
+        Customer customer = new Customer();
+        Menu menu = new Menu(List.of(new MenuItem("돈까스", 5000), new MenuItem("냉면", 7000)));
+        Cooking cooking = new Cooking();
 
+        Assertions.assertThatCode(() -> customer.order("돈까스", menu, cooking))
+                .doesNotThrowAnyException();
     }
 }
